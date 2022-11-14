ac4f81ff04a293a6c617bf77c102fa9c3c1b5d49
Stanislas Bruhiere <stanbill91@gmail.com>
2022-11-14 10:33:57 +0100

    Fix a and whitespaces

--- src/components/send/footer/footer.tsx
+++ src/components/send/footer/footer.tsx
@@ -3,7 +3,7 @@ import './footer.scss';
 const footerLinkAbout = <a href="https://pyronear.org/" target="_blank" rel="noopener noreferrer">à propos de Pyronear</a>
 const footerLinkWebsite = <a href="https://pyronear.org/" target="_blank" rel="noopener noreferrer">Site web</a>
 const footerLinkLegalMentions = <a href="https://pyronear.org/" target="_blank" rel="noopener noreferrer">mentions légales</a>
-const separator = <a> - </a>
+const separator = <p> - </p>
 
-export const Footer = () => 
+export const Footer = () =>
 <div id="footer">{footerLinkAbout}{separator}{footerLinkWebsite}{separator}{footerLinkLegalMentions}</div>
