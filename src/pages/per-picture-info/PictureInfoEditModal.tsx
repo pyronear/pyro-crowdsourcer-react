282aa2a633d0cd11793b1cfcdaa9950af1d4805f
Stanislas Bruhière <46960549+Nastaliss@users.noreply.github.com>
2024-03-11 05:53:36 +0100

    feat: check validity in datetimepicker and update it upstream (#8)
    * feat: check validity in datetimepicker and update it upstream
    
    * fix: remove clg

--- src/pages/per-picture-info/PictureInfoEditModal.tsx
+++ src/pages/per-picture-info/PictureInfoEditModal.tsx
@@ -17,10 +17,15 @@ export const PictureInfoEditModal: ModalInnerComponent = ({ close, context }: { 
     context.patchInfo({ datetime: date, department: department as string, consent: true })
     close()
   }
-  // todo : fix validity
+
+  const onDateTimeChange = ({ dateTime, valid }: { dateTime: Date, valid: boolean }): void => {
+    setDate(dateTime)
+    // todo : handle validity
+  }
+
   return (<>
     <FontAwesomeIcon icon={faCircleXmark} className='closeIcon' onClick={close}/>
-    <GlobalInfoForm date={date} onDateTimeChange={setDate} onDepartmentChange={(d) => setDepartment(d as string)} initialDepartment={context.department}/>
+    <GlobalInfoForm date={date} onDateTimeChange={onDateTimeChange} onDepartmentChange={(d) => setDepartment(d as string)} initialDepartment={context.department}/>
     <Button text='Valider' onClick={submit}/>
   </>)
 }
